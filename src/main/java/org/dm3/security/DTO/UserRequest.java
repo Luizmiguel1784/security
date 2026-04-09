b1004f646ec682e58c36f0a9d7a57be8750783c4
luizmiguel1784 <santosalmeidaluizmiguel28@gmail.com>
2026-04-09 20:02:19 -0300

    feat: create user DTO

---
+++ src/main/java/org/dm3/security/DTO/UserRequest.java
@@ -0,0 +1,14 @@
+package org.dm3.security.DTO;
+
+import lombok.Data;
+
+@Data
+public class UserRequest {
+
+    private String Name;
+    private String email;
+    private String Password;
+
+
+
+}
---
+++ src/main/java/org/dm3/security/DTO/UserResponse.java
@@ -0,0 +1,11 @@
+package org.dm3.security.DTO;
+
+import lombok.Data;
+
+@Data
+public class UserResponse {
+
+    private long id;
+
+
+}

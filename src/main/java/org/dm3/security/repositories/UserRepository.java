93a248e680e062eada0dd32f7400a73b4f57187f
luizmiguel1784 <santosalmeidaluizmiguel28@gmail.com>
2026-04-09 20:01:11 -0300

    feat: create user repositories

---
+++ src/main/java/org/dm3/security/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package org.dm3.security.repositories;
+
+import org.dm3.security.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByEmail(String email);
+}

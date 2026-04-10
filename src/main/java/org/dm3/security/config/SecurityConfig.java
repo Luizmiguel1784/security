15ae41a1b0de4af01be1abbb864fcadfb651abc8
luizmiguel1784 <santosalmeidaluizmiguel28@gmail.com>
2026-04-10 10:55:12 -0300

    fix: class name

--- src/main/java/org/dm3/security/config/SecurityConfig.java
+++ src/main/java/org/dm3/security/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package org.dm3.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,7 +12,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
-public class UserConfig {
+public class SecurityConfig {
 
 
     @Bean

8ff3fc07a5ed705974b123e684008afeb2a159e4
luizmiguel1784 <santosalmeidaluizmiguel28@gmail.com>
2026-04-23 21:29:04 -0300

    feat: auth manager

--- src/main/java/org/dm3/security/config/SecurityConfig.java
+++ src/main/java/org/dm3/security/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package org.dm3.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -26,6 +28,11 @@ public class SecurityConfig {
                 .anyRequest().authenticated()
                 ).httpBasic(withDefaults());
         return http.build();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+
+        return config.getAuthenticationManager();
     }
     
 
